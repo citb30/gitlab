e26204124e0192beb6c30b5341cb7af84a8c2551
carreerit <raghuk.vit@gmail.com>
2018-08-31 06:20:50 +0530

    Create install-gitlab.sh

---
+++ install-gitlab.sh
@@ -0,0 +1,7 @@
+IP=$(curl ifconfig.me)
+curl -sS https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.rpm.sh | sudo bash
+sudo EXTERNAL_URL="http://$IP" yum install -y gitlab-ce
+
+echo -e "You can loging to the server using following URL and with username root and setup the password during first login"
+
+echo -e "GitLab URL = http://$IP"
